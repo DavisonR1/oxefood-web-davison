496af14a6f453e0ebc0dd3719be421c36f72bdf8
unknown <drl1@discente.ifpe.br>
2023-04-27 20:35:52 -0300

    Aula 27/04

--- src/MenuSistema.jsx
+++ src/MenuSistema.jsx
@@ -22,12 +22,12 @@ class MenuSistema extends React.Component{
                        as={Link}
                        to='/'
                    />
-<Menu.Item
+                    <Menu.Item
                        name='cliente'
                        active={this.state.activeItem === 'cliente'}
                        onClick={this.handleItemClick}
                        as={Link}
-                       to='/form-cliente'
+                       to='/list-cliente'
                    />
 
                    <Menu.Item
@@ -43,7 +43,7 @@ class MenuSistema extends React.Component{
                        active={this.state.activeItem === 'entregador'}
                        onClick={this.handleItemClick}
                        as={Link}
-                       to='/form-entregador'
+                       to='/list-entregador'
                    />
 
                </Menu>
